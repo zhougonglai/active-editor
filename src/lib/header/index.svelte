9141f840b207df615d5afe9a7eb8727ee3e7a6a7
zhougonglai <zhougonglai@nn.com>
2022-04-02 00:25:17 +0800

    project init

---
+++ src/lib/header/index.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<header class="px-20">
+	<div class="logo flex items-center">
+		<img
+			src="https://www.leigod.com/img/logo.png"
+			height={42}
+			alt="雷神加速器"
+		/>
+	</div>
+</header>
+
+<style lang="scss" scoped>
+	header {
+		background-color: var(--bg);
+		height: 64px;
+		.logo {
+			height: inherit;
+			img {
+				height: 42px;
+			}
+		}
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,57 +3,17 @@
 </script>
 
 <script>
-	import Counter from '$lib/Counter.svelte';
+	const activities = [496];
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>首页</title>
 </svelte:head>
 
-<section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
+<section class="grid grid-cols-4">
+	{#each activities as active}
+		<div class="shadow-dark-500 rounded">
+			{active}
 		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
+	{/each}
 </section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
